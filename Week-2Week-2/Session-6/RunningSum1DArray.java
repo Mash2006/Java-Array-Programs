4ee002a0f5f82b827f3485ca28d95a1a16432225
KONETI MAHESH BABU <konatimaheshbabu@gmail.com>
2026-08-04 11:19:07 +0530

    Add files via upload

---
+++ Week-2Week-2/Session-6/RunningSum1DArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RunningSum1DArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        sc.close();
+    }
+}
