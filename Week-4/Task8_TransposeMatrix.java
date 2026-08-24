2bff6d73d8b5a6d6734092a7afdcc981a2288243
KONETI MAHESH BABU <197787033+Mash2006@users.noreply.github.com>
2026-08-24 12:14:35 +0530

    Add matrix transposition functionality
    Implement a method to transpose a matrix and display the result.

---
+++ Week-4/Task8_TransposeMatrix.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Task8_TransposeMatrix {
+
+    public static int[][] transpose(int[][] matrix) {
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] result = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < columns; j++) {
+
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] result = transpose(matrix);
+
+        for (int[] row : result) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
