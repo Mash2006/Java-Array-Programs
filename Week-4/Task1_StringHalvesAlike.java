1179b158a8c904b74a2479274684d762237aab18
KONETI MAHESH BABU <197787033+Mash2006@users.noreply.github.com>
2026-08-24 12:04:42 +0530

    Implement halvesAreAlike method for vowel comparison
    Added methods to check if two halves of a string have the same number of vowels.

--- Week-4/Task1_StringHalvesAlike.java
+++ Week-4/Task1_StringHalvesAlike.java
@@ -1,1 +1,39 @@
+public class Task1_StringHalvesAlike {
 
+    public static boolean halvesAreAlike(String s) {
+        int mid = s.length() / 2;
+        int firstCount = 0;
+        int secondCount = 0;
+
+        for (int i = 0; i < mid; i++) {
+            if (isVowel(s.charAt(i))) {
+                firstCount++;
+            }
+        }
+
+        for (int i = mid; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                secondCount++;
+            }
+        }
+
+        return firstCount == secondCount;
+    }
+
+    public static boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+
+        return ch == 'a' ||
+               ch == 'e' ||
+               ch == 'i' ||
+               ch == 'o' ||
+               ch == 'u';
+    }
+
+    public static void main(String[] args) {
+
+        String s = "book";
+
+        System.out.println(halvesAreAlike(s));
+    }
+}
